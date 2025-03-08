fe5db1d271e4159b60512deb9bd4210b7b84b8fa
Wesley0010012 <wglaurindo33@gmail.com>
2025-03-08 18:09:09 -0300

    feat: exporta componentes de uso do pacote

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { DocumentosEnum } from "@/main/enums/documentos-enum";
+import { Documentos } from "@/main/documentos";
+import { MetadadosDocumentoCPF } from "@/core/types/metadados-documento-cpf";
+import { MetadadosDocumentoCNPJ } from "@/core/types/metadados-documento-cnpj";
+
+export {
+  Documentos,
+  DocumentosEnum,
+  MetadadosDocumentoCPF,
+  MetadadosDocumentoCNPJ,
+};
